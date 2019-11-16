f0ada1521a5403ff687b3df85082489322d89680
Philipp323 <philipp-auinger@gmx.at>
2019-11-16 12:54:11 +0100

    Person hinzugefuegt

---
+++ quarkus-panache-supermarket/src/main/java/at/htl/supermarket/model/Person.java
@@ -0,0 +1,29 @@
+package at.htl.supermarket.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn
+public class Person extends PanacheEntity {
+
+    public String firstname;
+    public String lastname;
+    public LocalDate birthdate;
+    public String mobilePhone;
+    public String email;
+
+    public Person() {
+    }
+
+    public Person(String firstname, String lastname, LocalDate birthdate, String mobilePhone, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthdate = birthdate;
+        this.mobilePhone = mobilePhone;
+        this.email = email;
+    }
+}
