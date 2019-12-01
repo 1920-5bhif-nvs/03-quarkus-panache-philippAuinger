8f8738aa5a94e9be15b18c423c009f65447a06e6
Philipp323 <philipp-auinger@gmx.at>
2019-12-01 21:19:52 +0100

    GET Request implementiert

--- quarkus-panache-supermarket/src/main/java/at/htl/supermarket/boundary/CustomerEndpoint.java
+++ quarkus-panache-supermarket/src/main/java/at/htl/supermarket/boundary/CustomerEndpoint.java
@@ -1,16 +1,30 @@
 package at.htl.supermarket.boundary;
 
 import at.htl.supermarket.business.CustomerRepository;
+import at.htl.supermarket.model.Customer;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("customer")
 public class CustomerEndpoint {
 
     @Inject
     CustomerRepository customerRepository;
-    
+
+    @GET
+    @Path("loyality")
+    public Response getAllSortedByLoyality(){
+        List<Customer> responseList = customerRepository.getAllSortedByLoyalityPoints();
+        if(responseList!=null) {
+            return Response.ok().entity(responseList).build();
+        } else {
+            return Response.noContent().build();
+        }
+    }
 }
