d795c05c76993a5b247930c6e6ad30efc1025224
Philipp323 <philipp-auinger@gmx.at>
2019-12-02 10:08:02 +0100

    ProductRepository PanacheRepository implementiert

--- quarkus-panache-supermarket/src/main/java/at/htl/supermarket/business/ProductRepository.java
+++ quarkus-panache-supermarket/src/main/java/at/htl/supermarket/business/ProductRepository.java
@@ -1,4 +1,11 @@
 package at.htl.supermarket.business;
 
-public class ProductRepository {
+import at.htl.supermarket.model.Product;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ProductRepository implements PanacheRepository<Product> {
+    
 }
