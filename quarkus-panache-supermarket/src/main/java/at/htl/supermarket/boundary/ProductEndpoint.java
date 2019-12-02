eca00350cb14ffa1b0233b178ada49b8bf7894c8
Philipp323 <philipp-auinger@gmx.at>
2019-12-02 10:05:45 +0100

    ProductRepository hinzugefuegt

--- quarkus-panache-supermarket/src/main/java/at/htl/supermarket/boundary/ProductEndpoint.java
+++ quarkus-panache-supermarket/src/main/java/at/htl/supermarket/boundary/ProductEndpoint.java
@@ -1,4 +1,7 @@
 package at.htl.supermarket.boundary;
 
+import javax.ws.rs.Path;
+
+@Path("product")
 public class ProductEndpoint {
 }
---
+++ quarkus-panache-supermarket/src/main/java/at/htl/supermarket/business/ProductRepository.java
@@ -0,0 +1,4 @@
+package at.htl.supermarket.business;
+
+public class ProductRepository {
+}
