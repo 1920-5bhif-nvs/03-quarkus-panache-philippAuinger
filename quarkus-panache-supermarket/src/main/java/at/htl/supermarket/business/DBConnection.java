6d16465e7037f8fec3928bff856af1cb2f74de93
Philipp323 <philipp-auinger@gmx.at>
2019-12-02 20:53:54 +0100

    DBConnection.java implementiert

--- quarkus-panache-supermarket/src/main/java/at/htl/supermarket/business/DBConnection.java
+++ quarkus-panache-supermarket/src/main/java/at/htl/supermarket/business/DBConnection.java
@@ -1,4 +1,18 @@
 package at.htl.supermarket.business;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private Connection dbConnection;
+
+    public void getDBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        dbConnection = DriverManager.getConnection("jdbc:postgresql://192.168.99.100:5432/postgres", "postgres", "passme");
+    }
+
+    public int executeQuery(String query) throws ClassNotFoundException, SQLException {
+        return dbConnection.createStatement().executeUpdate(query);
+    }
 }
