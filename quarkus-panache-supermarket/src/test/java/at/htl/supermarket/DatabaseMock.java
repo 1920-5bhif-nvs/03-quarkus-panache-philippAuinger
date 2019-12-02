ef98e60b49e57b5f577ec169053df303d62ae801
Philipp323 <philipp-auinger@gmx.at>
2019-12-02 21:03:06 +0100

    Mock implementiert

--- quarkus-panache-supermarket/src/test/java/at/htl/supermarket/DatabaseMock.java
+++ quarkus-panache-supermarket/src/test/java/at/htl/supermarket/DatabaseMock.java
@@ -1,4 +1,38 @@
 package at.htl.supermarket;
 
+import at.htl.supermarket.business.DBConnection;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class DatabaseMock {
+    @InjectMocks
+    private DBConnection dbConnection;
+    @Mock
+    private Connection mockConnection;
+    @Mock
+    private Statement mockStatement;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testMockDBConnection() throws Exception {
+        Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
+        Mockito.when(mockConnection.createStatement().executeUpdate(Mockito.any())).thenReturn(1);
+        int value = dbConnection.executeQuery("");
+        Assertions.assertEquals(value,1);
+        Mockito.verify(mockConnection.createStatement(), Mockito.times(1));
+    }
 }
