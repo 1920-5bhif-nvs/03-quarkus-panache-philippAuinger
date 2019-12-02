e5ab9a3d635e7cf87175790b5ef6d5398fa204bb
Philipp323 <philipp-auinger@gmx.at>
2019-12-02 13:01:39 +0100

    CashierRepository save() implementiert

--- quarkus-panache-supermarket/src/main/java/at/htl/supermarket/business/CashierRepository.java
+++ quarkus-panache-supermarket/src/main/java/at/htl/supermarket/business/CashierRepository.java
@@ -1,4 +1,15 @@
 package at.htl.supermarket.business;
 
-public class CashierRepository {
+import at.htl.supermarket.model.Cashier;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class CashierRepository implements PanacheRepository<Cashier> {
+
+    public Cashier save(Cashier newCashier){
+        persistAndFlush(newCashier);
+        return findById(newCashier.id);
+    }
 }
