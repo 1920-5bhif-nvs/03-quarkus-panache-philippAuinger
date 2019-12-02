e909da1a9df1b471f2394ac16b2bc56ecc16e444
Philipp323 <philipp-auinger@gmx.at>
2019-12-02 10:17:59 +0100

    ProductEndpoint implementiert

--- quarkus-panache-supermarket/src/main/java/at/htl/supermarket/boundary/ProductEndpoint.java
+++ quarkus-panache-supermarket/src/main/java/at/htl/supermarket/boundary/ProductEndpoint.java
@@ -1,8 +1,28 @@
 package at.htl.supermarket.boundary;
 
+import at.htl.supermarket.business.ActivityRepository;
+import at.htl.supermarket.model.Activity;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("product")
 public class ProductEndpoint {
-    
+
+    @Inject
+    ActivityRepository activityRepository;
+
+    @GET
+    @Path("soldProducts")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getSoldProductsSorted(){
+        List<Activity> responseList = activityRepository.getSoldProductsSortedByDate();
+
+        return Response.ok().entity(responseList).build();
+    }
 }
